7b709fd8a65d0a3b20e42f74be266eecbe5e36d1
Putri Defa <putri@aqi.co.id>
2023-07-04 14:27:35 +0700

    home

--- App.tsx
+++ App.tsx
@@ -15,30 +15,152 @@ import {
   Pressable,
   Badge,
   Spacer,
-  Flex
+  Flex,
+  ScrollView,
+  Avatar,
+  VStack
 } from "native-base";
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-function AppBar() {
-  return <>
-      <StatusBar backgroundColor="#3700B3" barStyle="light-content" />
-      <Box safeAreaTop bg="violet.600" />
-      <HStack bg="violet.600" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%">
-        <HStack alignItems="center">
-          <IconButton icon={<Icon name="ios-reorder-three" size={30} color="#FFF" />} />
-          <Text color="white" fontSize="20" fontWeight="bold">
-            Home
-          </Text>
-        </HStack>
-      </HStack>
-    </>;
+// header
+// function AppBar() {
+//   return <Center>
+//       <Box _dark={{bg: 'coolGray.800'}} _light={{bg: 'white' }} flex="1" safeAreaTop maxW="400px" w="100%">
+//         <Box bg="white">
+//           <HStack space={3} pt="12" pl="4" pb="12" size="md">
+//             <Avatar size="48px" source={{uri: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}} />
+//             <VStack>
+//               <Text fontSize="xs" color="coolGray.800" _dark={{color: 'warmGray.50'}} alignSelf="flex-start">
+//                 Hallo
+//               </Text>
+//               <Heading>
+//                 Afreen Khan
+//               </Heading>
+//             </VStack>
+//           </HStack>
+//        </Box>
+//       </Box>
+//     </Center>;
+// }
+// header
+
+// banner
+function Banner() {
+  return 
 }
+// banner
+
+// content
+function Content() {
+  return <Center h="735px">
+      <Box _dark={{bg: 'coolGray.800'}} _light={{bg: 'white' }} flex="1" safeAreaTop maxW="400px" w="100%">
+      <Box bg="white">
+        <HStack space={3} pt="12" pl="4" pb="12" size="md">
+          <Avatar size="48px" source={{
+            uri: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+          }} />
+            <VStack>
+            <Text fontSize="xs" color="coolGray.800" _dark={{color: 'warmGray.50'}} alignSelf="flex-start">
+              Hallo
+            </Text>
+            <Heading>
+                Afreen Khan
+              </Heading>
+            </VStack>
+        </HStack>
+        </Box>
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <Box alignItems="center" bg="white">
+        <Image source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} alt="Alternate Text"  width={370} height={147} />
+        </Box>
+        <Stack pt="4" pl="4" pr="4" pb="12" space={3}>
+          <Stack space={2}>
+            <Heading size="md">
+               Breakthrough Study Reveals Potential Cure for Alzheimer's Disease
+            </Heading>
+          </Stack>
+        </Stack>
+        <Box pl="4" pr="5" py="2">
+          <HStack alignItems="center" space={3} bg="white">
+          <Image source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} alt="Alternate Text"  width={90} height={60} />
+            <VStack>
+            <Text fontSize="xs" color="coolGray.800" _dark={{color: 'warmGray.50'}} alignSelf="flex-start">
+              12:47 PM
+            </Text>
+              <Text color="coolGray.800" _dark={{color: 'warmGray.50'}} bold isTruncated maxW="350" w="80%">
+                World Leaders Gather for Global Climate Summit, Pledge Bold Actions
+              </Text>
+            </VStack>
+            <Spacer />
+          </HStack>
+        </Box>
+        <Box pl="4" pr="5" py="2">
+          <HStack alignItems="center" space={3} bg="white">
+          <Image source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} alt="Alternate Text"  width={90} height={60} />
+            <VStack>
+            <Text fontSize="xs" color="coolGray.800" _dark={{color: 'warmGray.50'}} alignSelf="flex-start">
+              12:47 PM
+            </Text>
+              <Text color="coolGray.800" _dark={{color: 'warmGray.50'}} bold isTruncated maxW="350" w="80%">
+                World Leaders Gather for Global Climate Summit, Pledge Bold Actions
+              </Text>
+            </VStack>
+            <Spacer />
+          </HStack>
+        </Box>
+        <Box pl="4" pr="5" py="2">
+          <HStack alignItems="center" space={3} bg="white">
+          <Image source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} alt="Alternate Text"  width={90} height={60} />
+            <VStack>
+            <Text fontSize="xs" color="coolGray.800" _dark={{color: 'warmGray.50'}} alignSelf="flex-start">
+              12:47 PM
+            </Text>
+              <Text color="coolGray.800" _dark={{color: 'warmGray.50'}} bold isTruncated maxW="350" w="80%">
+                World Leaders Gather for Global Climate Summit, Pledge Bold Actions
+              </Text>
+            </VStack>
+            <Spacer />
+          </HStack>
+        </Box>
+        </ScrollView>
+      </Box>
+    </Center>;
+}
+// content
+
+// footer
+function Footer() {
+  const [selected, setSelected] = React.useState(1);
+  return <NativeBaseProvider>
+      <Box flex={1} bg="white" width="100%" alignSelf="center">
+        <HStack bg="#C40B0B" alignItems="center">
+          <Pressable opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
+            <Center>
+              <IconButton icon={<Icon name="home-outline" size={25} color="white" />} variant={"ghost"} colorScheme="indigo" />
+            </Center>
+          </Pressable>
+          <Pressable opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
+            <Center>
+              <IconButton icon={<Icon name="search" size={25} color="white" />} variant={"ghost"} colorScheme="indigo" />
+            </Center>
+          </Pressable>
+          <Pressable opacity={selected === 3 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(3)}>
+            <Center>
+              <IconButton icon={<Icon name="person-outline" size={25} color="white" />} variant={"ghost"} colorScheme="indigo" />
+            </Center>
+          </Pressable>
+        </HStack>
+      </Box>
+    </NativeBaseProvider>;
+}
+// footer
 
 export default () => {
   return (
     <NativeBaseProvider>
-          <AppBar />
+          <Content />
+          <Footer />
     </NativeBaseProvider>
   );
 };
